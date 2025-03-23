78bd7f4ef2d47ca3aadae0b709152671c4b9922a
min-perilla <szm1027170042@outlook.com>
2025-03-23 18:11:30 +0800

    amplysis for Web 1.2

--- start.R
+++ start.R
@@ -1,16 +1,16 @@
 # start.R
+# 2025-03-23
 
-# 安装 R 包
-if (!requireNamespace("plumber", quietly = TRUE)) {
-  install.packages("plumber")
-}
+rm(list = ls())
 
-# 加载 R 包
-library(plumber)
+# 记录原始库路径
+original_libpaths <- .libPaths()
 
 # 设置工作目录
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-cat(getwd())
+
+# 确保 R 优先从本地 library 目录加载包
+.libPaths(c(file.path(getwd(), "library"), .libPaths()))
 
 # 启动 UI
 browseURL("UI/index.html")
@@ -18,3 +18,6 @@ browseURL("UI/index.html")
 # 启动 API
 pr <- plumber::plumb("API/api.R")
 pr$run(port = 8000)
+
+# 恢复原始的库路径
+.libPaths(original_libpaths)
